7e13a5889a88f73a0a2a580836e39dc78944f593
Rachana Sudhakar <rachana@Rachanas-MacBook-Air.local>
2026-07-29 16:13:55 -0400

    Add code file upload API

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import shutil
+import os
+
 
 app = FastAPI(
     title="AI Code Review Assistant"
 )
 
 
+UPLOAD_FOLDER="app/uploads"
+
+
 @app.get("/")
 def home():
+
     return {
-        "message":"AI Code Review Assistant API Running"
+        "message":"AI Code Review Assistant Running"
+    }
+
+
+
+@app.post("/upload")
+async def upload_code(
+    file: UploadFile = File(...)
+):
+
+    file_path=f"{UPLOAD_FOLDER}/{file.filename}"
+
+
+    with open(file_path,"wb") as buffer:
+
+        shutil.copyfileobj(
+            file.file,
+            buffer
+        )
+
+
+    return {
+
+        "filename":file.filename,
+        "status":"uploaded"
+
     }
